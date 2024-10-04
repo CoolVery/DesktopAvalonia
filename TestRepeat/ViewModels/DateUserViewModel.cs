a179bb63e814259f8eeb4b8c2b7732f218cd947d
CoolVery <gorgn2005@gmail.com>
2024-10-04 09:55:25 +0300

    ended make update user data

--- TestRepeat/ViewModels/DateUserViewModel.cs
+++ TestRepeat/ViewModels/DateUserViewModel.cs
@@ -1,24 +1,41 @@
+using Avalonia.Collections;
 using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using TestRepeat.Models;
+using TestRepeat.Views;
 
 namespace TestRepeat.ViewModels
 {
 	public partial class DateUserViewModel : ViewModelBase
 	{
 		[ObservableProperty] Logined currentUser;
+		[ObservableProperty] string dateString;
 		[ObservableProperty] List<Gender> genders;
 		public DateUserViewModel() { }
 		public DateUserViewModel(Logined user) {
 			currentUser = user;
 			genders = MainWindowViewModel.Db_context.Genders.ToList();
-		}
+			dateString = currentUser.User.BirthDate.ToString();
+
+        }
 
 		public void SaveChange()
 		{
 			MainWindowViewModel.Db_context.SaveChanges();
+		}
+		public DateTimeOffset NewDate { 
+			get => new DateTimeOffset((DateTime)CurrentUser.User.BirthDate, TimeSpan.Zero);
+			set
+			{
+				CurrentUser.User.BirthDate = new DateTime(value.Year, value.Month, value.Day);
+                DateString = value.ToString();
+            }
+		}
+		public void BackToAuth()
+		{
+			MainWindowViewModel.Instance.Uc = new Authorization();
 		}
     }
 }

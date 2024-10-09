71a74242bf4cde8be1d225bc7d24a1e68f25d1d7
CoolVery <georgekuklevwork@gmail.com>
2024-10-09 21:15:46 +0300

    ended make not visible delete-button

---
+++ TestRepeat/Models/UserCustom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRepeat.Models
+{
+    public class UserCustom : User
+    {
+        public bool IsCanseled { get; set; } = true;
+    }
+}
--- TestRepeat/ViewModels/InfoUsersDateViewModel.cs
+++ TestRepeat/ViewModels/InfoUsersDateViewModel.cs
@@ -7,63 +7,88 @@ using TestRepeat.Views;
 
 namespace TestRepeat.ViewModels
 {
-	public partial class InfoUsersDateViewModel : ViewModelBase
-	{
-		[ObservableProperty] List<User> listUsers;
-		[ObservableProperty] List<Gender> listGenders;
-		[ObservableProperty] string textFolderContent;
-		[ObservableProperty] Gender selectedGender;
+    public partial class InfoUsersDateViewModel : ViewModelBase
+    {
+        [ObservableProperty] List<UserCustom> listUsers;
+        [ObservableProperty] List<Gender> listGenders;
+        [ObservableProperty] string textFolderContent;
+        [ObservableProperty] Gender selectedGender;
 
-        List<User> copyUsers;
-		public InfoUsersDateViewModel() { }
-        public InfoUsersDateViewModel(List<User> users) {
-            this.listUsers = users;
-            copyUsers = users;
+        List<UserCustom> copyUsers;
+        public InfoUsersDateViewModel() { }
+        public InfoUsersDateViewModel(List<User> users)
+        {
+            listUsers = ConvertList(users);
+            copyUsers = listUsers;
             listGenders = [
                 new Gender() { Gender1 = "—бросить фильтр", IdGender = 0 },
                 .. users.Select(gender => gender.IdGenderNavigation).Distinct().OrderBy(gender => gender.IdGender).ToList()
                 ];
         }
-		public void DateSort(int idParam)
-		{
-			switch (idParam)
-			{
-				case 1:
-					ListUsers = ListUsers.OrderByDescending(x => x.BirthDate).ToList();
-					break;
-				case 2:
-					ListUsers = ListUsers.OrderBy(x => x.BirthDate).ToList();
-					break;
-			}
-		}
+        public void DateSort(int idParam)
+        {
+            switch (idParam)
+            {
+                case 1:
+                    ListUsers = ListUsers.OrderByDescending(x => x.BirthDate).ToList();
+                    break;
+                case 2:
+                    ListUsers = ListUsers.OrderBy(x => x.BirthDate).ToList();
+                    break;
+            }
+        }
+        private List<UserCustom> ConvertList(List<User> users)
+        {
+            List<UserCustom> listUsers = new List<UserCustom>();
+            foreach (var user in users)
+            {
+                UserCustom tempUser =
+                    new UserCustom
+                    {
+                        Name = user.Name,
+                        IdGender = user.IdGender,
+                        IdGenderNavigation = user.IdGenderNavigation,
+                        IdUser = user.IdUser,
+                        IdUserNavigation = user.IdUserNavigation,
+                        BirthDate = user.BirthDate,
 
-		partial void OnSelectedGenderChanged(Gender value) => Filters();
+                    };
+                if (user.IdUser == InfoUsersDate.CurrentUser.IdUser)
+                {
+                    tempUser.IsCanseled = false;
+                }
+                listUsers.Add(tempUser);
+            }
+            return listUsers;
+        }
+        partial void OnSelectedGenderChanged(Gender value) => Filters();
         partial void OnTextFolderContentChanged(string value) => Filters();
         void Filters()
-		{
-			ListUsers = copyUsers;
+        {
+            ListUsers = copyUsers;
             if (!string.IsNullOrEmpty(textFolderContent)) ListUsers = ListUsers.Where(user => user.Name.Contains(textFolderContent)).ToList();
-			if(selectedGender != null && selectedGender.IdGender != 0) ListUsers = ListUsers.Where(user => user.IdGenderNavigation == selectedGender).ToList();
-		}
-		public void ChangeUserDate(int idUser)
-		{
-			User changeableUser = MainWindowViewModel.Db_context.Users.FirstOrDefault(user => user.IdUser == idUser);
+            if (selectedGender != null && selectedGender.IdGender != 0) ListUsers = ListUsers.Where(user => user.IdGenderNavigation == selectedGender).ToList();
+        }
+        public void ChangeUserDate(int idUser)
+        {
+            User changeableUser = MainWindowViewModel.Db_context.Users.FirstOrDefault(user => user.IdUser == idUser);
             MainWindowViewModel.Instance.Uc = new DateUser(InfoUsersDate.CurrentUser, changeableUser);
         }
-		public void DeleteUser(int idUser) {
-			MainWindowViewModel.Db_context
-				.Remove(
-					MainWindowViewModel.Db_context.Users.FirstOrDefault(u => u.IdUser == idUser));
-			MainWindowViewModel.Db_context
-				.Remove(
-					MainWindowViewModel.Db_context.Logineds.FirstOrDefault(l => l.Id == idUser));
+        public void DeleteUser(int idUser)
+        {
+            MainWindowViewModel.Db_context
+                .Remove(
+                    MainWindowViewModel.Db_context.Users.FirstOrDefault(u => u.IdUser == idUser));
+            MainWindowViewModel.Db_context
+                .Remove(
+                    MainWindowViewModel.Db_context.Logineds.FirstOrDefault(l => l.Id == idUser));
             MainWindowViewModel.Db_context.SaveChanges();
-			MainWindowViewModel.Instance.Uc = new InfoUsersDate(MainWindowViewModel.Db_context.Users.ToList());
-		}
-		public void AddNewUser()
-		{
-			MainWindowViewModel.Instance.Uc = new NewUser();
-		}
+            MainWindowViewModel.Instance.Uc = new InfoUsersDate(MainWindowViewModel.Db_context.Users.ToList());
+        }
+        public void AddNewUser()
+        {
+            MainWindowViewModel.Instance.Uc = new NewUser();
+        }
 
     }
 }
